61682c26c7ab83b48cacc3a281f5529c73d4dc2a
Timofey Maximov <moltenship7750@gmail.com>
2023-07-08 14:03:24 +0600

    feat: add metatags

--- src/app/last/page.tsx
+++ src/app/last/page.tsx
@@ -1,6 +1,23 @@
+import { type Metadata } from 'next';
+
 import { RaceService } from '@/api/services/race';
 import { DataTable } from '@/components/DataTable';
 import { columns } from '@/components/RaceResultsTable';
+
+export async function generateMetadata(): Promise<Metadata> {
+  const lastRace = await RaceService.getLastRaceResults();
+  return {
+    description: `Results for latest race ${lastRace.raceName} season ${lastRace.season} round ${lastRace.round}`,
+    openGraph: {
+      description: `Results for latest race ${lastRace.raceName} season ${lastRace.season} round ${lastRace.round}`,
+      siteName: 'F1 App',
+      title: `Results for ${lastRace.raceName} — F1 App`,
+      type: 'website',
+      url: `https://f1-app-eight.vercel.app/last`,
+    },
+    title: `Results for ${lastRace.raceName}`,
+  };
+}
 
 const LastRacePage = async() => {
   const lastRace = await RaceService.getLastRaceResults();
