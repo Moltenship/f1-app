f9f3d20b5120bb2b8a5d2d5670cef6b440e2947f
Timofey Maximov <moltenship7750@gmail.com>
2023-07-08 14:48:30 +0600

    feat: update icon

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,11 +1,18 @@
-import { FlagIcon } from 'lucide-react';
+import Image from 'next/image';
 
 export const Header = () => (
   <header className="border-b">
     <div className="container py-4 flex items-center">
       <a href="/">
-        <div className="flex items-center gap-2">
-          <FlagIcon />
+        <div className="flex items-center gap-2 text-inherit">
+          <Image
+            alt="Logo"
+            className="dark:invert"
+            height={24}
+            priority
+            src="/logo.svg"
+            width={24}
+          />
           <h1 className="scroll-m-20 font-semibold tracking-tight">
             F1 App
           </h1>
