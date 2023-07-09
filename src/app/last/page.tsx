59a981c5c11ae04eb223138cb2384c3513a52753
Timofey Maximov <moltenship7750@gmail.com>
2023-07-09 12:03:34 +0600

    feat: improve results page

--- src/app/last/page.tsx
+++ src/app/last/page.tsx
@@ -3,6 +3,7 @@ import { type Metadata } from 'next';
 import { RaceService } from '@/api/services/race';
 import { DataTable } from '@/components/DataTable';
 import { columns } from '@/components/RaceResultsTable';
+import { TableHeading } from '@/components/ui/Table';
 
 export async function generateMetadata(): Promise<Metadata> {
   const lastRace = await RaceService.getLastRaceResults();
@@ -25,9 +26,10 @@ const LastRacePage = async() => {
   return (
     <main className="md:container">
       <div className="flex flex-col gap-4">
-        <h1 className="text-2xl font-semibold leading-none tracking-tight text-center">
-          {lastRace.raceName}
-        </h1>
+        <TableHeading
+          description={`Season ${lastRace.season}, round ${lastRace.round}. ${lastRace.Circuit.circuitName}`}
+          title={lastRace.raceName}
+        />
         <DataTable columns={columns} data={lastRace.Results} />
       </div>
     </main>
